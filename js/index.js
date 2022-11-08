247f8bb33a894fc2a71c04607e894b44f4cd668c
Muhammad Huzaifa <mhuzaifa134@gmail.com>
2022-11-09 01:09:43 +0500

    import sound changes

--- js/index.js
+++ js/index.js
@@ -1,8 +1,10 @@
 console.log("this is a game");
 let inputDir = { x: 0, y: 0 };
+import sound from "../music/move.mp3";
+const moveSound =  new Audio(sound);
 const foodSound = new Audio("../music/food.mp3");
 const gameOverSound = new Audio("../music/gameover.mp3");
-const moveSound = new Audio("../music/move.mp3");
+// const moveSound = new Audio("../music/move.mp3");
 const musicSound = new Audio("../music/music.mp3");
 let speed = 10;
 let score = 0;
